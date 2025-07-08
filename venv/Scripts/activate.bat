19068aac4fe3c0ef25b7af05be65b4206a8a3e9c
sagarika0309 <lrnrfrvr@gmail.com>
2025-07-08 23:36:31 +0530

    initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ADITYA\OneDrive\Documents\RetailMate\RetailMate\venv
+set VIRTUAL_ENV=C:\Users\misra\Documents\walmart_sparkathon\RetailMate\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
